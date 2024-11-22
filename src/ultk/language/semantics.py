bb6a24a6301aa7a5833ea96a9579e92749ee56df
Ashvin Ranjan <ranjanashvin@gmail.com>
2024-11-22 14:57:44 -0800

    Fix some issues with incorrect definitions - Fix type definition from `frozenset[Referent, float]` to `frozenset[tuple[Referent, float]]` - Fix `frozenset` definitions

--- src/ultk/language/semantics.py
+++ src/ultk/language/semantics.py
@@ -145,19 +145,19 @@ class Meaning(Generic[T]):
     # With the mapping, `universe` is not conceptually needed, but it is very useful to have it lying around.
     # `universe` should be the keys to `mapping`. 
     universe: Universe
-    _dist: frozenset[Referent, float] = frozenset({})
+    _dist: frozenset[tuple[Referent, float]] = frozenset({})
 
     @property
-    def dist(self) -> frozenset[Referent, float]:
+    def dist(self) -> frozenset[tuple[Referent, float]]:
         if self._dist:
             # normalize weights to distribution
             total_weight = sum([v for _, v in self._dist])
-            return frozenset({referent: weight / total_weight for referent, weight in self._dist})
+            return frozenset({(referent, weight / total_weight) for referent, weight in self._dist})
         else:
             num_true_like = sum(1 for value in self.mapping.values() if value)
             if num_true_like == 0:
                 raise ValueError("Meaning must have at least one true-like referent.")
-            return frozenset({referent: (1 / num_true_like if self.mapping[referent] else 0) for referent in self.mapping})
+            return frozenset({(referent, (1 / num_true_like if self.mapping[referent] else 0)) for referent in self.mapping})
         
     def is_uniformly_false(self) -> bool:
         """Return True if all referents in the meaning are mapped to False (or coercible to False).In the case where the meaning type is boolean, this corresponds to the characteristic function of the empty set."""
